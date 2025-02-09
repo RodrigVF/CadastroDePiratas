e4dc47a8e44c04ae6664d77a0a2b9dfc8eb93510
rodrigovilelaferreira <rod.vf@hotmail.com>
2025-02-09 09:58:25 -0300

    first commit

---
+++ src/test/java/com/rodrigvf/CadastroDePiratas/CadastroDePiratasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigvf.CadastroDePiratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePiratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
