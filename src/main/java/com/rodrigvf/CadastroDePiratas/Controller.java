a9efbdc8c90d97b474b3c88a1b0115ca2de961fd
rodrigovilelaferreira <rod.vf@hotmail.com>
2025-02-10 11:38:17 -0300

    Create controller

---
+++ src/main/java/com/rodrigvf/CadastroDePiratas/Controller.java
@@ -0,0 +1,18 @@
+package com.rodrigvf.CadastroDePiratas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas() {
+        return "Bem vindo(a) à API de cadastro Pirata";
+    }
+
+
+
+}
