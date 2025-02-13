c8b6d2f8370b37d2810b2a976077a5eebf9f6707
RodrigVF <rod.vf@hotmail.com>
2025-02-13 15:27:46 -0300

    feat: Update controller with mocks

--- src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataController.java
+++ src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataController.java
@@ -1,8 +1,6 @@
 package com.rodrigvf.CadastroDePiratas.Piratas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,33 @@ public class PirataController {
         return "Bem vindo(a) à API de cadastro Pirata";
     }
 
+    // Adicionar (CREATE)
+    @PostMapping("/criar")
+    public String criarPirata() {
+        return "Pirata criado com sucesso!";
+    }
+
+    // Mostrar todos os piratas (READ)
+    @GetMapping("/all")
+    public String mostrarTodosPiratas() {
+        return "Mostrar pirata!";
+    }
+
+    // Procurar Pirata por ID (READ)
+    @GetMapping("/allID")
+    public String mostrarPirataId() {
+        return "Mostrar pirata por ID!";
+    }
+
+    // Alterar dados dos piratas (UPDATE
+    @PutMapping("/alterarID")
+    public String alterarPirataId() {
+        return "Pirata atualizado com sucesso!";
+    }
+
+    // Deletar Pirata (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarPirataId() {
+        return "Pirata deletado com sucesso!";
+    }
 }
