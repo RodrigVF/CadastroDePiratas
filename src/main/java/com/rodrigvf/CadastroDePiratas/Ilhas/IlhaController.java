2fe60ca298c03bdfe9c2ea369c06b51abfdd2460
RodrigVF <rod.vf@hotmail.com>
2025-02-13 15:44:26 -0300

    feat: Create mock routes of IlhaController

--- src/main/java/com/rodrigvf/CadastroDePiratas/Ilhas/IlhaController.java
+++ src/main/java/com/rodrigvf/CadastroDePiratas/Ilhas/IlhaController.java
@@ -1,4 +1,29 @@
 package com.rodrigvf.CadastroDePiratas.Ilhas;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("ilhas")
 public class IlhaController {
+
+    @GetMapping("/listar")
+    public String listarIlha() {
+        return "Ilhas listadas com sucesso!";
+    }
+
+    @PostMapping("/criar")
+    public String criarIlha() {
+        return "Ilha criada com sucesso!";
+    }
+
+    @PutMapping("/alterarID")
+    public String atualizarIlha() {
+        return "Ilha alterada com sucesso!";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarIlha() {
+        return "Ilha deletada com sucesso!";
+    }
+
 }
