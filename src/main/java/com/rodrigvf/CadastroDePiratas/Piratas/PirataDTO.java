05cd437ee43e0a34ad2fff667bba554b669d798e
RodrigVF <rod.vf@hotmail.com>
2025-02-14 14:20:01 -0300

    chore: implements DTO and Mapper for piratas

--- src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataDTO.java
+++ src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataDTO.java
@@ -1,4 +1,21 @@
 package com.rodrigvf.CadastroDePiratas.Piratas;
 
+import com.rodrigvf.CadastroDePiratas.Ilhas.IlhaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PirataDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private double recompensa;
+    private IlhaModel ilhas;
+
 }
