2554cac35c3a302c6239a65a7a37a80599bbcf41
RodrigVF <rod.vf@hotmail.com>
2025-02-20 14:38:08 -0300

    doc: Implements swagge for document routes of application

--- src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataController.java
+++ src/main/java/com/rodrigvf/CadastroDePiratas/Piratas/PirataController.java
@@ -1,5 +1,9 @@
 package com.rodrigvf.CadastroDePiratas.Piratas;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +21,19 @@ public class PirataController {
     }
 
     @GetMapping("/boas-vindas")
+    @Operation(summary = "Mensagem de boas vindas", description = "Essa rota retorna mensagem de boas vindas para testar se API está rodando")
     public String boasVindas() {
         return "Bem vindo(a) à API de cadastro Pirata";
     }
 
     // Adicionar (CREATE)
     @PostMapping("/criar")
+    @Operation(summary = "Criar um novo pirata", description = "Essa rota é utilizada para criar um novo pirata e inserir no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pirata criado com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar pirata")
+    })
+
     public ResponseEntity<String> criarPirata(@RequestBody PirataDTO pirata) {
 
         PirataDTO novoPirata = pirataService.criarPirata(pirata);
@@ -33,6 +44,12 @@ public class PirataController {
 
     // Mostrar todos os piratas (READ)
     @GetMapping("/listar")
+    @Operation(summary = "Criar um novo pirata", description = "Rota para criar um novo pirata e inserir no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pirata criado com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar pirata")
+    })
+
     public ResponseEntity<List<PirataDTO>> listarPiratas() {
 
         List<PirataDTO> piratas =  pirataService.listarPiratas();
@@ -42,6 +59,12 @@ public class PirataController {
 
     // Procurar Pirata por ID (READ)
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista um pirata por Id", description = "Rota lista pirata pelo seu Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pirata encontrado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Pirata não encontrado")
+    })
+
     public ResponseEntity<?> buscarPirataPorId(@PathVariable Long id) {
 
         PirataDTO pirata = pirataService.buscarPirataPorId(id);
@@ -57,7 +80,17 @@ public class PirataController {
 
     // Alterar dados dos piratas (UPDATE)
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarPirata(@PathVariable Long id, @RequestBody PirataDTO pirataAtualizado) {
+    @Operation(summary = "Altera pirata por Id", description = "Rota altera pirata pelo seu Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pirata alterado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Pirata não encontrado, não foi possivel realizar alterações")
+    })
+    public ResponseEntity<?> alterarPirata(
+        @Parameter(description = "Necessário enviar ID na URL da requisição")
+        @PathVariable Long id,
+        @Parameter(description = "Necessário enviar os dados do pirata a ser atualizado no corpo(body) da requisição")
+        @RequestBody PirataDTO pirataAtualizado) {
+
 
         PirataDTO pirata = pirataService.alterarPirata(id, pirataAtualizado);
 
@@ -72,6 +105,11 @@ public class PirataController {
 
     // Deletar Pirata (DELETE)
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deletar pirata por Id", description = "Rota lista o pirata pelo seu Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pirata encontrado com sucesso!"),
+            @ApiResponse(responseCode = "404", description = "Pirata não encontrado")
+    })
     public ResponseEntity<String> deletarPirataPorId(@PathVariable Long id) {
 
         if (pirataService.buscarPirataPorId(id) != null) {
